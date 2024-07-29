5702aff9ceccd723548f0ad8c18e83fc70193a42
SriMoorthy <srimoorthy27@gmail.com>
2024-07-29 18:39:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Your code here.
+const items = document.querySelector('.items')
+let isDown = false;
+let startX;
+let scorollLeft;
+
+items.addEventListener('mousedown',(e)=>{
+	isDown = true;
+    items.classList.add('active');
+    startX = e.pageX - items.offsetLeft;
+    scrollLeft = items.scrollLeft;
+})
+
+items.addEventListener('mouseleave', () => {
+    isDown = false;
+    items.classList.remove('active');
+});
